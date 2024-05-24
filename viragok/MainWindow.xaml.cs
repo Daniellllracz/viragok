25229ee3a5ff581a06af8fe5718989a33dc5826e
Daniellllracz <78079867+Daniellllracz@users.noreply.github.com>
2024-05-24 09:31:08 +0200

    Add project files.

---
+++ viragok/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace viragok
+{
+    public partial class MainWindow : Window
+    {
+        List<Flowers> flowers;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            flowers = new List<Flowers>();
+            var sr = new StreamReader("../../../src/flowers.txt");
+            while (!sr.EndOfStream)
+            {
+                flowers.Add(new Flowers(sr.ReadLine(), ";"));
+            }
+            sr.Close();
+
+            listbox.ItemsSource = flowers;
+            listbox.SelectionChanged += Listbox_SelectionChanged;
+        }
+        private void Listbox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (listbox.SelectedItem is Flowers selectedFlower)
+            {
+                string imagePath = Path.Combine("../../../src/img", selectedFlower.ImagePath);
+                var bitmap = new BitmapImage(new Uri(imagePath, UriKind.Relative));
+                var tempImage = bitmap;
+                imgFlower.Source = tempImage;
+            }
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            if (listbox.SelectedItem is Flowers selectedFlower)
+            {
+                flowers.Remove(selectedFlower);
+                listbox.Items.Refresh();
+                imgFlower.Source = null;
+            }
+        }
+    }
+}
---
+++ viragok/flowers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace viragok
+{
+    internal class Flowers
+    {
+        public string? Name { get; set; }
+        public int Price { get; set; }
+        public string Color { get; set; }
+        public string ImagePath { get; set; }
+
+        public Flowers(string line, string separator)
+        {
+            string[] data = line.Split(separator);
+            Name = data[0];
+            Price = int.Parse(data[1]);
+            Color = data[2];
+            ImagePath = data[3];
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Price: {Price}, Color: {Color}";
+        }
+    }
+}
